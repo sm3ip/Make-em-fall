2cf666b141440d585b670f46e18052300eb3156c
sm3ip <lucasomenpc@gmail.com>
2023-09-06 16:49:31 +0200

    balls disappear after a certain amount of time

--- Assets/scripts/BallControl.cs
+++ Assets/scripts/BallControl.cs
@@ -1,13 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BallControl : MonoBehaviour
 {
+    public int deathCountDown;
+
+    public void Update()
+    {
+        deathCountDown -= 1;
+        if (deathCountDown<0)
+        {
+            death();
+        }
+    }
 
     public void ThrowBall(Vector3 dir, float strength)
     {
         gameObject.transform.parent = null;
         GetComponent<Rigidbody>().AddForce(dir * strength, ForceMode.Impulse);
+    }
+
+    private void death()
+    {
+        Destroy(gameObject);
     }
 }
